5d125ae13a7f75cfc3c3d86a71a23b88f4e5212d
Christian Nascimento <cnascimento@ciandt.com>
2021-11-20 00:21:13 -0300

    Add webpack instead of rollup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./Home";
+import Aside from "./components/Aside";
 
 function App() {
-  return <h1>Hello World From React-Rollup</h1>;
+  return (
+    <>
+      <NavBar />
+      <main className="container px-4 mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 py-8">
+        <Home />
+        <Aside />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./style.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
