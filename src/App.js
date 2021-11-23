8a0b4d88001c92fb6435faad6aa8de6b0ddf43a7
Christian Nascimento <cnascimento@ciandt.com>
2021-11-22 23:19:15 -0300

    Add provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./Home";
+import Home from "./routes/home";
 import Aside from "./components/Aside";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <NavBar />
-      <main className="container px-4 mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 py-8">
-        <Home />
+      <main className="container px-4 mx-auto grid grid-cols-1 md:grid-cols-3 md:gap-8 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
         <Aside />
       </main>
-    </>
+    </Provider>
   );
 }
 
