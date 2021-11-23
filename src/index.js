1a863b2d2ca560aa487684fe6a08335eceb90762
Christian Nascimento <cnascimento@ciandt.com>
2021-11-22 23:18:00 -0300

    Add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Home from "./routes/home";
+
 import "./style.css";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const rootElement = document.getElementById("root");
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);
